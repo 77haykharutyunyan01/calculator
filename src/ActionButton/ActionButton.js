2816ab793474eef039645e62a3153c8bf3ae518c
Hayk Harutyunyan <hayk_harutyunyan_2001@mail.ru>
2023-09-01 12:00:54 +0400

    calculator

---
+++ src/ActionButton/ActionButton.js
@@ -0,0 +1,26 @@
+import "./action-button.css"
+const ActionButton = () => {
+    return (
+        <div className="div">
+            <button className="c">&#x2201;</button>
+            <button className="less-than-sign">&#x3c;</button>
+            <button className="division-slash">&#x2215;</button>
+            <button className="multiplication-sign">&#xd7;</button>
+            <button className="seven">&#x2077;</button>
+            <button className="eight">&#x2078;</button>
+            <button className="nine">&#x2079;</button>
+            <button className="minus-sign">&#x2212;</button>
+            <button className="four">&#x2074;</button>
+            <button className="five">&#x2075;</button>
+            <button className="six">&#x2076;</button>
+            <button className="plus-sign">&#x2b;</button>
+            <button className="one">&#xb9;</button>
+            <button className="two">&#xb2;</button>
+            <button className="three">&#xb3;</button>
+            <button className="equal-sign">&#x3d;</button>
+            <button className="zero">&#x2070;</button>
+            <button className="dot-operator">&#x22C5;</button>
+        </div>
+    )
+}
+export default ActionButton
---
+++ src/ScoreBoard/ScoreBoard.js
@@ -0,0 +1,5 @@
+import "./score-board.css"
+const ScoreBoard = () => {
+    return  <input className="input" type="text" placeholder="0"/> 
+}
+export default ScoreBoard
